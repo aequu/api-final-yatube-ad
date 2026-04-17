014f340c2ccf040d4be9fb18e7ccf4cb460bd56d
Ян <yan.gorodetskij@bk.rui>
2026-04-17 17:52:08 +0500

    errors fix

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -98,10 +98,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    'DEFAULT_PAGINATION_CLASS': 
+    ],
+    'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
-    ],
 }
 
 DJOSER = {
